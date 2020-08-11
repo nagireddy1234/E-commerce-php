9231d1516aae45d2f453a6ce29043c707f1819f9
nagiReddy <nagireddy.panditi@gmail.com>
2020-08-11 14:16:18 +0530

    converted html pages to php

---
+++ cart.php
@@ -0,0 +1,15 @@
+<?php 
+include('header.php');
+
+/*  include cart */
+include ('./Template/_cart-template.php');
+/*  include cart */
+
+/*  include new phones section  */
+include ('./Template/_new-phones.php');
+/*  include new phones section  */
+?>
+
+<?php 
+include('footer.php');
+?>
---
+++ product.php
@@ -0,0 +1,16 @@
+<?php 
+include('header.php');
+
+/*  include product */
+include ('./Template/_products.php');
+/*  include product */
+
+
+/*  include top sale section */
+include ('./Template/_top-sale.php');
+/*  include top sale section */
+?>
+
+<?php 
+include('footer.php');
+?>
